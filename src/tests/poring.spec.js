798bae29a3b1950b518a0a0de2a046a8e93363f8
Carlos Fdez. Llamas <hello@sirikon.me>
2024-04-16 20:22:52 +0200

    add some tests

--- src/tests/poring.spec.js
+++ src/tests/poring.spec.js
@@ -1,50 +1,80 @@
-const { useSignal, useEffect, useComputed, h } = poring;
+const { useSignal, useEffect, useComputed, h, runScope } = poring;
 
 describe("Signals", () => {
-  it("can do all the basic functions", () => {
-    poring
-      .runScope({}, () => {
-        const signal = useSignal("a");
-        assertEqual(signal.get(), "a");
-        signal.set("b");
-        assertEqual(signal.get(), "b");
-      })
-      .dispose();
+  it("can be getted and setted", () => {
+    runScope({}, () => {
+      const signal = useSignal("a");
+      assertEqual(signal.get(), "a");
+      signal.set("b");
+      assertEqual(signal.get(), "b");
+    }).dispose();
+  });
+  it("can be subscribed and unsubscribed", () => {
+    runScope({}, () => {
+      const signal = useSignal(0);
+      let execCount = 0;
+      const handler = () => {
+        execCount++;
+      };
+      signal.subscribe(handler);
+      assertEqual(execCount, 0);
+      signal.set((s) => s + 1);
+      assertEqual(execCount, 1);
+      signal.unsubscribe(handler);
+      signal.set((s) => s + 1);
+      assertEqual(execCount, 1);
+    }).dispose();
   });
 });
 
 describe("Effects", () => {
-  it("re-executes when signals change", () => {
-    poring
-      .runScope({}, () => {
-        const signal = useSignal("a");
-        const otherSignal = useSignal("a");
-        let execCount = 0;
-        useEffect(() => {
-          signal.get();
-          execCount++;
-        });
-        assertEqual(execCount, 1);
-        signal.set("b");
-        assertEqual(execCount, 2);
-        otherSignal.set("b");
-        assertEqual(execCount, 2);
-      })
-      .dispose();
+  it("re-executes when signals change ignoring other signals", () => {
+    runScope({}, () => {
+      const signal = useSignal("a");
+      const otherSignal = useSignal("a");
+      let execCount = 0;
+      useEffect(() => {
+        signal.get();
+        execCount++;
+      });
+      assertEqual(execCount, 1);
+      signal.set("b");
+      assertEqual(execCount, 2);
+      otherSignal.set("b");
+      assertEqual(execCount, 2);
+    }).dispose();
+  });
+  it("updates the depended signals", () => {
+    runScope({}, () => {
+      const number = useSignal(0);
+      const boolean = useSignal(true);
+      let execCount = 0;
+      useEffect(() => {
+        if (boolean.get()) {
+          number.get();
+        }
+        execCount++;
+      });
+      assertEqual(execCount, 1);
+      boolean.set(false);
+      assertEqual(execCount, 2);
+      number.set((n) => n + 1);
+      assertEqual(execCount, 2);
+      boolean.set(true);
+      assertEqual(execCount, 3);
+    }).dispose();
   });
 });
 
 describe("Computed", () => {
   it("re-calculates when signals change", () => {
-    poring
-      .runScope({}, () => {
-        const signal = useSignal(1);
-        const computedSignal = useComputed(() => signal.get() * 2);
-        assertEqual(computedSignal.get(), 2);
-        signal.set(2);
-        assertEqual(computedSignal.get(), 4);
-      })
-      .dispose();
+    runScope({}, () => {
+      const signal = useSignal(1);
+      const computedSignal = useComputed(() => signal.get() * 2);
+      assertEqual(computedSignal.get(), 2);
+      signal.set(2);
+      assertEqual(computedSignal.get(), 4);
+    }).dispose();
   });
 });
 
